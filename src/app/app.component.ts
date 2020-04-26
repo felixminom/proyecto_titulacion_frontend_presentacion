e9dce16595cf98917c5c1a14d6b6d69f6ffc2586
felixminom <felix.minom@gmail.com>
2020-04-25 19:34:42 -0500

    Angular Material funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSliderModule} from '@angular/material/slider';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend-presentacion';
+  title = 'Visualizacion';
 }
